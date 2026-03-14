b52f9ed284fe7ce42133f4e493562ffaf7ad1cbc
Diego Carmona <diegoncarmona@gmail.com>
2026-03-14 08:56:30 -0300

    feat: enhance EditPostModal with custom scrollbar styles and update textarea layout

--- app/features/posts/EditPostModal.tsx
+++ app/features/posts/EditPostModal.tsx
@@ -48,14 +48,30 @@ export default function EditPostModal({
       <label className="block text-label mb-1" htmlFor="edit-content">
         Content
       </label>
-      <div className="input-field mb-4 overflow-hidden p-0 focus-within:outline focus-within:outline-gray">
+      <div className="relative border border-gray rounded-lg py-1 px-2 w-full focus:outline-gray mb-6 overflow-hidden p-0 focus-within:outline focus-within:outline-gray">
         <textarea
           id="edit-content"
-          className="text-input resize-y h-[50dvh] md:h-fit min-h-20 max-h-[50vh] w-full focus:outline-none"
+          className="text-input custom-scrollbar resize-y h-[50dvh] md:h-fit min-h-20 max-h-[50vh] w-full focus:outline-none"
           value={content}
           maxLength={4096}
           onChange={(e) => setContent(e.target.value)}
         />
+
+        <div className="pointer-events-none absolute bottom-2 right-2 text-gray text-2xl select-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 10 10"
+          >
+            <path
+              d="M9 4L4 9M9 7L7 9"
+              stroke="currentColor"
+              stroke-width="1.2"
+              stroke-linecap="round"
+            />
+          </svg>
+        </div>
       </div>
 
       <div className="flex justify-end gap-4">
